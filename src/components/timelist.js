bf0c660f2a9e9057c1bae883644b10a0a4ca4b04
Bmilith <96339417+Bmilith@users.noreply.github.com>
2022-04-10 14:15:06 +0200

    mob respons

--- src/components/timelist.js
+++ src/components/timelist.js
@@ -10,49 +10,55 @@ function timelist() {
     }, 2500);
   }
   return (
-    <div className="ColumnTime">
-      <button class="popup button6" onClick={() => myFunction("myPopup")}>
-        8 : 00 h
-        <span class="popuptext" id="myPopup">
-          Uspješna rezervacija!
-        </span>
-      </button>
-      <button class="popup button6" onClick={() => myFunction("myPopup9")}>
-        9 : 00 h
-        <span class="popuptext" id="myPopup9">
-          Uspješna rezervacija!
-        </span>
-      </button>
-      <button class="popup button6" onClick={() => myFunction("myPopup2")}>
-        10 : 00 h
-        <span class="popuptext" id="myPopup2">
-          Uspješna rezervacija!
-        </span>
-      </button>
-      <button class="popup button6" onClick={() => myFunction("myPopup3")}>
-        12 : 20 h
-        <span class="popuptext" id="myPopup3">
-          Uspješna rezervacija!
-        </span>
-      </button>
-      <button class="popup button6" onClick={() => myFunction("myPopup3")}>
-        15 : 35 h
-        <span class="popuptext" id="myPopup3">
-          Uspješna rezervacija!
-        </span>
-      </button>
-      <button class="popup button6" onClick={() => myFunction("myPopup5")}>
-        17 : 35 h
-        <span class="popuptext" id="myPopup5">
-          Uspješna rezervacija!
-        </span>
-      </button>
-      <button class="popup button6" onClick={() => myFunction("myPopup4")}>
-        18 : 35 h
-        <span class="popuptext" id="myPopup4">
-          Uspješna rezervacija!
-        </span>
-      </button>
+    <div className="ColumnTime" id="mob">
+      <div className="mobrow">
+        <button class="popup button6" onClick={() => myFunction("myPopup")}>
+          8 : 00 h
+          <span class="popuptext" id="myPopup">
+            Uspješna rezervacija!
+          </span>
+        </button>
+        <button class="popup button6" onClick={() => myFunction("myPopup9")}>
+          9 : 00 h
+          <span class="popuptext" id="myPopup9">
+            Uspješna rezervacija!
+          </span>
+        </button>
+        <button class="popup button6" onClick={() => myFunction("myPopup2")}>
+          10 : 00 h
+          <span class="popuptext" id="myPopup2">
+            Uspješna rezervacija!
+          </span>
+        </button>
+      </div>
+      <div className="mobrow">
+        <button class="popup button6" onClick={() => myFunction("myPopup3")}>
+          12 : 20 h
+          <span class="popuptext" id="myPopup3">
+            Uspješna rezervacija!
+          </span>
+        </button>
+        <button class="popup button6" onClick={() => myFunction("myPopup3")}>
+          15 : 35 h
+          <span class="popuptext" id="myPopup3">
+            Uspješna rezervacija!
+          </span>
+        </button>
+        <button class="popup button6" onClick={() => myFunction("myPopup5")}>
+          17 : 35 h
+          <span class="popuptext" id="myPopup5">
+            Uspješna rezervacija!
+          </span>
+        </button>
+      </div>
+      <div className="mobrow2">
+        <button class="popup button6" onClick={() => myFunction("myPopup4")}>
+          18 : 35 h
+          <span class="popuptext" id="myPopup4">
+            Uspješna rezervacija!
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
